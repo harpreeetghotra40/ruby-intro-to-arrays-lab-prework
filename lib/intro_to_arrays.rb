8360b3b2b70642a59c4902ef5587ff0ed46dc151
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-10 22:49:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,12 @@ def instantiate_new_array
   newArray = []
   return newArray
 end
+
+def my_two_array
+  newArray = ["make" , "this"]
+  return newArray
+end
+
+def first_element(newArray)
+  return newArray.first
+end
